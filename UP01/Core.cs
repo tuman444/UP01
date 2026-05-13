194526d9c42e16f61352e24870641d1422553ac6
tuman <tuman@DIMA>
2026-05-13 12:30:31 +0300

    Подключение бд

---
+++ UP01/Core.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP01
+{
+    public class Core
+    {
+        private static JokeAndKingEntities _db;
+
+        public static JokeAndKingEntities DB => GetContext();
+
+        public static JokeAndKingEntities GetContext()
+        {
+            if (_db == null)
+            {
+                _db = new JokeAndKingEntities();
+            }
+            return _db;
+        }
+
+    }
+}
